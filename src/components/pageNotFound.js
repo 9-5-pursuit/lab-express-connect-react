5b0b7a5b154c8409db861886bc48ceba168499d6
Yanako Lajara <yanakolajara@pursuit.org>
2023-06-30 19:53:13 -0400

    Error page fixed

--- src/components/pageNotFound.js
+++ src/components/pageNotFound.js
@@ -1,7 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
 export function PageNotFound(){
+    const navigator = useNavigate()
     return(
-        <p>Page not found</p>
+        <div class="mainContent">
+            <h1>Error 404</h1>
+            <h2>Page not found</h2>
+            <button
+            id="errorButton"
+            onClick={() => navigator('/logs')}
+            >Back to homepage</button>
+        </div>
     )
 }
