a4f3dac6460577338f8803ba55b7b6e6616e5ef5
Yanako Lajara <yanakolajara@pursuit.org>
2023-06-30 13:44:55 -0400

    new log form done

--- src/components/create-log/createLog.js
+++ src/components/create-log/createLog.js
@@ -3,16 +3,66 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export function CreateLog(){
-    // TODO: Create grid for form
+    const [name, setName] = useState('')
+    const [title, setTitle] = useState('')
+    const [post, setPost] = useState('')
+    const [crisis, setCrisis] = useState('')
+    const [mistakes, setMistakes] = useState('')
+    async function submitForm(){
+        try{
+            await axios.post()
+        }catch(e){
+            console.log(e)
+        }
+    }
     return(
         <div class="mainContent">
-            <div class="contentBox" id="createLog">
+            <h2>New</h2>
+            <div id="createLog">
                 <form>
-                    <label htmlFor="newName">Name</label>
+                    <label htmlFor="newName">Captain's Name:</label>
+                    <br/>
                     <input
                     type="text"
                     id="newName"
+                    onChange={(x) => setName(x)}
                     />
+                    <br/>
+                    <label htmlFor="newTitle">Title:</label>
+                    <br/>
+                    <input
+                    type="text"
+                    id="newTitle"
+                    onChange={(x) => setTitle(x)}
+                    />
+                    <br/>
+                    <label htmlFor="newPost">Post:</label>
+                    <br/>
+                    <input
+                    type="text"
+                    id="newPost"
+                    onChange={(x) => setPost(x)}
+                    />
+                    <br/>
+                    <label htmlFor="newCrisis">Days Since Last Crisis:</label>
+                    <br/>
+                    <input
+                    type="number"
+                    id="newCrisis"
+                    onChange={(x) => setCrisis(x)}
+                    />
+                    <br/>
+                    <label htmlFor="newMistakes">Mistakes were made today:</label>
+                    <br/>
+                    <input
+                    type="checkbox"
+                    id="newMistakes"
+                    onChange={(x) => setMistakes(x)}
+                    />
+                    <button
+                    id="newPost"
+                    onChange={(x) => console.log(x)}
+                    >Submit</button>
                 </form>
             </div>
         </div>
