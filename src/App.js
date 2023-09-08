037dfbb09266c79dc1896b02402dabb54c98f05a
YasserS-21 <saadi.yasser04@gmail.com>
2023-09-08 10:47:56 -0400

    Functional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./Components/Nav/Nav";
+import Logs from "./Components/Logs/Logs"
+import Log from "./Components/Logs/Log"
+import EditLog from "./Components/EditLog/editlog";
+import CreateLog from "./Components/CreateLog/createlog";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+  <Router>
+    <Nav />
+    <Routes>
+      <Route path="/logs" element={<Logs/>} />
+      <Route path="/logs/:id" element={<Log/>}/>
+      <Route path="/logs/:id/edit" element={<EditLog/>}/>
+      <Route path="/logs/new" element={<CreateLog/>}/>
+    </Routes>
+
+
+  </Router>
+  )
 }
 
 export default App;
---
+++ src/Components/CreateLog/createlog.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+function CreateLog() {
+  const navigate = useNavigate()
+  const [log, setLog] = useState({})
+
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    try {
+        let result = await axios.post("http://localhost:3001/logs", {
+            captainName: log.captainName,
+            title: log.title,
+            post: log.post,
+            mistakesWereMadeToday: log.mistakesWereMadeToday,
+            daysSinceLastCrisis: log.daysSinceLastCrisis,
+        })
+       
+        setLog({})
+        navigate("/logs")
+    } catch (e) {
+        console.log(e)
+    }
+  }
+
+  return (
+    <form onSubmit={(e) => handleOnSubmit(e)}>
+        <h2>New Log</h2>
+        <br />
+        <label>Captain's Name:</label>
+        <input type="text" value={log?.captainName} onChange={(e) => setLog({ ...log, captainName: e.target.value})}required />
+        <br />
+        <label>Title</label>
+        <input type="text" value={log?.title} onChange={(e) => setLog({ ...log, title: e.target.value})} required
+        />
+        <br />
+        <label>Post:</label>
+        <input type="text" value={log?.post} onChange={(e) => setLog({ ...log, post: e.target.value})} placeholder="What happened today?" required/>
+        <br />
+        <label>Days Since Last Crisis</label>
+        <input type="number" value={log?.daysSinceLastCrisis} onChange={(e) => setLog({...log, daysSinceLastCrisis: e.target.value})} required/>
+        <br />
+        <label>Mistakes were made today?</label>
+        <input type="checkbox" value={log.mistakesWereMadeToday} onChange={(e) => setLog({ ...log,mistakesWereMadeToday: e.target.value})}/>
+        <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default CreateLog
